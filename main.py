176c0630b642453cbd9bef48831cc9167ceba97a
lq0000 <root@lo.calho.st>
2016-05-24 13:30:51 -0600

    prioritized routes for urgent traffic

--- main.py
+++ main.py
@@ -8,15 +8,15 @@ def main():
 
         # build computation layer
         #compute = ImportedTopology(ft.datacenter.fat_tree_topology(4))
-	compute = ImportedTopology(ft.simplemodels.full_mesh_topology(10))
+        compute = ImportedTopology(ft.simplemodels.full_mesh_topology(10))
         compute.prefixAllNodes("/direct/com")
         compute.prefixAllNodes("/overlay/com")
         compute.labelAllNodes("compute")
         compute.labelAllEdges("compute")
-	
+        
         # build aggregation layer
         aggrega = ImportedTopology(ft.randmodels.barabasi_albert_topology(64, 2, 8))
-	#aggrega = ImportedTopology(ft.randmodels.barabasi_albert_topology(100, 2, 10))
+        #aggrega = ImportedTopology(ft.randmodels.barabasi_albert_topology(100, 2, 10))
         aggrega.prefixAllNodes("/direct/agg")
         aggrega.prefixAllNodes("/overlay/agg")
         aggrega.labelAllNodes("aggregate")
@@ -38,7 +38,7 @@ def main():
         agg_nodes = sorted([x for x in joined.nodeSet if "aggregate" in x.labels], key=lambda x: x.getDegree())
         
         # introduce physical layer
-	for i in xrange(0, 100):
+        for i in xrange(0, 100):
         #for i in xrange(0, 1000):
                 # create phy node
                 n = joined.addNode("phy_%d" % i, labels=["physical"])
@@ -47,24 +47,42 @@ def main():
                 # choose agg node to connect to
                 a = agg_nodes[int(pow(random.random(), 2) * len(agg_nodes))]
                 joined.addEdge(n, a, label="overlay")
-
-	# export the network topology to a file
-	allnodes = joined.ExportTopology()
-	      
-        # compute routes
+                
+        # find tentative routes
+        t = IcnRoutes(joined)
+        t.restrictPrefix("/direct", ["normal", "overlay"])
+        t.restrictPrefix("/overlay", ["overlay"])
+        t.calculateRoutes()
+    
+        # restrict agg nodes' shortest paths to com to urgent traffic
+        for n in agg_nodes:
+            face, cost = t.hops[n]["/direct/com"]
+            if face != None:
+                joined.getEdge(n, face).label = "urgent"
+        
+        # add urgent prefix to com nodes
+        for n in joined.nodeSet:
+            if "compute" in n.labels:
+                n.addPrefix("/urgent/com")
+        
+        # export the network topology to a file
+        allnodes = joined.ExportTopology()
+              
+        # compute final routes
         r = IcnRoutes(joined)
+        r.restrictPrefix("/urgent", ["normal", "overlay", "urgent"])
         r.restrictPrefix("/direct", ["normal", "overlay"])
         r.restrictPrefix("/overlay", ["overlay"])
+        r.calculateRoutes()
        
         # ensure all prefixes are reachable from all nodes
-        r.calculateRoutes()
         for node, pd in r.hops.items():
                 for prefix, (face, hops) in pd.items():
                         if hops == None:
                                 print("%s can't reach %s" % (node, prefix))
-                        #print("%s -> %s : %s : %f" % (node, prefix, face, hops))
+                        print("%s -> %s : %s : %f" % (node, prefix, face, hops))
 
-	# export the routing tables for the nodes to a file
+        # export the routing tables for the nodes to a file
         r.exportroutingtables(allnodes)
 
 main()
